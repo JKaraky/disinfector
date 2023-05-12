722e89b4150dc0d14087b755db52e162b2623768
MostafaH-01 <mostafa.harb01@gmail.com>
2023-05-12 16:43:51 +0300

    Fixed Hoop And Added Sound

--- Assets/Scripts/HoopCollision.cs
+++ Assets/Scripts/HoopCollision.cs
@@ -10,10 +10,14 @@ public class HoopCollision : MonoBehaviour
     [SerializeField]
     public List<Transform> transforms = new List<Transform>();
 
+    public AudioClip scoresound;
+    AudioSource audio;
+
     Transform newTrans;
     // Start is called before the first frame update
     void Start()
     {
+        audio = GetComponent<AudioSource>();
         stateChange = StateChange.instance;
 
         newTrans = transforms[Random.Range(0, transforms.Count)];
@@ -32,8 +36,13 @@ public class HoopCollision : MonoBehaviour
         if (other.gameObject.layer == 6) //Layer 6 is Pickable
         {
             stateChange.score++;
+            audio.PlayOneShot(scoresound, 1f);
 
             newTrans = transforms[Random.Range(0, transforms.Count)];
+            // If the random transform picked is the same as the previous, get another random one
+            while (newTrans.position == transform.position)
+                newTrans = transforms[Random.Range(0, transforms.Count)];
+
             Debug.Log(newTrans);
             transform.position = newTrans.position;
             transform.rotation = newTrans.rotation;
